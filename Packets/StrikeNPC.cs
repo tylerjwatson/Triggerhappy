61bad8a77dda508cf286b22396dd51898fb3726b
Tyler Watson <tyler@tw.id.au>
2014-05-15 23:51:23 +1000

    beginning unsafe packet struct decoding

---
+++ Packets/StrikeNPC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TriggerHappy.Packets {
+    [StructLayout(LayoutKind.Sequential, Pack=0x1)]
+    unsafe struct StrikeNPC {
+        public short NPCID;
+        public byte PlayerID;
+
+        public static unsafe StrikeNPC MarshalFromBuffer(ref TerrariaApi.Server.GetDataEventArgs args) {
+            StrikeNPC npc;
+
+            fixed (byte* _buffer = args.Msg.readBuffer) {
+                byte *bufferOffset = _buffer + args.Index;
+                npc = *((StrikeNPC*)bufferOffset);
+            }
+
+            return npc;
+        }
+    }
+}
